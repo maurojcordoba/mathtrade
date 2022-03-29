43c5725eec28a390a0395ef53c860ffae9579e02
maurojcordoba <mauro.j.cordoba@gmail.com>
2022-03-29 18:19:55 -0300

    archivo faltantes

--- app.py
+++ app.py
@@ -1,7 +1,4 @@
 from flask import Flask, render_template,request
-from flaskext.mysql import MySQL
-from database import DataBase
-import json
 from databasemath import DataBaseMath
 
 app = Flask(__name__)
@@ -29,57 +26,6 @@ def games():
         
     return render_template('games.html', data=data ,title=title,url_template=url_template) 
 
-
-@app.route('/custom')
-def custom():
-    title = 'Custom'
-
-    database = DataBase()
-    items = database.get_custom_query()
-    
-    data = items    
-    return render_template('custom.html', data=data ,title=title)
-    
-@app.route('/buscalibre')
-def buscalibre():
-    title = 'BuscaLibre'
-
-    url_template = 'https://www.buscalibre.com.ar/amazon?url=https%3A%2F%2Fwww.amazon.com%2Fdp%2F{}&t=tetraemosv4'
-
-    database = DataBase()
-    items = database.get_bl_items()
-    
-    data = items 
-    return render_template('buscalibre.html', data=data ,title=title,url_template=url_template) 
-
-@app.route('/tiendamia')
-def tiendamia():
-    title = 'TiendaMia'
-    
-    url_template = 'https://tiendamia.com/ar/producto?amz={}'
-        
-    database = DataBase()
-    items = database.get_tm_items()
-    
-    data = items 
-    return render_template('tiendamia.html', data=data ,title=title,url_template=url_template) 
-
-@app.route('/bd')
-def bd():
-    title = 'BookDepository'
-    
-    url_template = 'https://www.bookdepository.com{}'
-        
-    database = DataBase()
-    items = database.get_bd_items()
-    
-    data = items 
-    return render_template('tiendamia.html', data=data ,title=title,url_template=url_template) 
-
-
-@app.route('/about')
-def about():
-    return render_template('about.html')
 
 def query_string():
     print(request)
